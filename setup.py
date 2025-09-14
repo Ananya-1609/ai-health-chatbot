d36dc6a183bb90df1da89edfd1627f65f6eb2c66
Ananya-1609 <saiananya1609@gmail.com>
2025-09-15 01:09:26 +0530

    floder structure edit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Health Chatbot',
+    version= '0.0.0',
+    author= 'Suguri Sai Ananya',
+    author_email= 'saiananya1609@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
